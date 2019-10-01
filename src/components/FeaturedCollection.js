a93ec19c63948f680aa5d51764f4c654d7b4bc32
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-10-01 22:48:11 +0200

    Moved data from FeaturedCollection.js to Main

--- src/components/FeaturedCollection.js
+++ src/components/FeaturedCollection.js
@@ -2,45 +2,10 @@ import React from "react";
 
 import FeaturedPost from "./FeaturedPost";
 
-export default () => {
-  //TO-DO: Replace placeholders
-  const postsPlaceholder = [
-    {
-      id: "001",
-      content: {
-        title: "Featured Title 1",
-        tagline: "Featured Tagline 1",
-      },
-      img: {
-        fileName: "2.jpg",
-        description: "alt 1",
-      },
-    },
-    {
-      id: "002",
-      content: {
-        title: "Featured Title 2",
-        tagline: "Featured Tagline 2",
-      },
-      img: {
-        fileName: "3.jpg",
-        description: "alt 2",
-      },
-    },
-    {
-      id: "003",
-      content: {
-        title: "Featured Title 3",
-        tagline: "Featured Tagline 3",
-      },
-      img: {
-        fileName: "4.jpg",
-        description: "alt 3",
-      },
-    },
-  ];
+export default props => {
+  const { posts } = props;
 
-  const featuredPosts = postsPlaceholder.map(item => <FeaturedPost key={item.id} content={item.content} img={item.img} />);
+  const featuredPosts = posts.map(item => <FeaturedPost key={item.id} content={item.content} img={item.img} />);
 
   return (
     <section>
