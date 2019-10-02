0fbecb32a05c8dc960a8e0c0c83fb80943fe117a
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-10-02 08:01:49 +0200

    Populate Footer with SocialMediaLIst

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React from "react";
 
-import Logo from "../components/Logo"
+import Logo from "../components/Logo";
+import SocialMediaList from "../components/SocialMediaList";
 
 export default () => {
   return (
     <footer>
       <Logo />
+      <SocialMediaList />
     </footer>
   )
 }
