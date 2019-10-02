aa552463cf82546435bc9937767be09fe77d9790
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-10-02 08:17:36 +0200

    Populate Footer with LanguageSwitch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Logo from "../components/Logo";
 import SocialMediaList from "../components/SocialMediaList";
+import LanguageSwitch from "../components/LanguageSwitch";
 
 export default () => {
   return (
     <footer>
       <Logo />
       <SocialMediaList />
+      <LanguageSwitch />
     </footer>
-  )
-}
+  );
+};
